5dbe05ffc2e9eb0d58ff6ac5cfc7a01bf77ca50a
odorable-code <voidkknd17@gmail.com>
2025-12-01 13:20:56 +0900

    JAVA 01일차 예제

---
+++ java/day01/EX01_HelloWorld.java
@@ -0,0 +1,8 @@
+package day01;
+
+public class EX01_HelloWorld {
+    public static void main(String[] args) {
+        // 표준 입력을 이용하여 콘솔에 문자열을 출력
+        System.out.println("Hello World!");
+    }
+}
