2230903f707f10c6183586c2090f098175c6d547
odorable-code <voidkknd17@gmail.com>
2025-12-10 16:54:45 +0900

    JAVA 08일차

--- java/homework/HW08_BaseballGame.java
+++ java/homework/HW08_BaseballGame.java
@@ -1,5 +1,8 @@
 package homework;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Random;
 import java.util.Scanner;
 
 public class HW08_BaseballGame {
@@ -24,63 +27,135 @@ public class HW08_BaseballGame {
 		 * 입력 : 1 5 4
 		 * 정답입니다.
 		 * */
-        // 답 생성
-        int [] answer = generateAnswer();
-        Scanner scan = new Scanner(System.in);
-        while (true) {
-            int [] userChoice = getUserChoice(scan);
-            int [] ballCount = ballCheck(answer, userChoice);
-            if (isStrikeOut(ballCount)) { break; }
-        }
+
+		ArrayList<Integer> com = new ArrayList<Integer>();
+		ArrayList<Integer> user = new ArrayList<Integer>();
+		
+		int min = 1, max = 9, size = 3;
+		int strike = 0, ball = 0;
+		//랜덤수 생성
+		try {
+			generateRandomList(com, min, max, size);
+			System.out.println(com);
+		}catch (Exception e) {
+			System.err.println(e.getMessage());
+			System.out.println("[프로그램 종료]");
+			return;
+		}
+		
+		//반복문 
+		do {
+			System.out.print("입력 : ");
+			//숫자 입력
+			if(!inputInteger(user, min, max, 3)) {
+				System.out.println("중복된 수를 입력했습니다.");
+				continue;
+			}
+			//스트라이크 개수 확인
+			strike = getStrike(com, user);
+			//볼 개수 확인
+			ball = getBall(com, user);
+			//결과 출력
+			printResult(strike, ball);
+		}while(strike < 3);
+		System.out.println("정답입니다.");
+			
+	}
+	
+	public static int play() {
+		return 0;
+	}
+	
+	private static int getBall(ArrayList<Integer> com, ArrayList<Integer> user) {
+		int total = 0;
+		for(Integer tmp : com) {
+			if(user.contains(tmp)) {
+				total++;
+			}
+		}
+		return total - getStrike(com, user);
+	}
+	private static int getStrike(ArrayList<Integer> com, ArrayList<Integer> user) {
+		int strike = 0;
+		for(int i = 0; i < com.size(); i++) {
+			if(com.get(i) == user.get(i)) {
+				strike++;
+			}
+		}
+		return strike;
+	}
+	public static boolean inputInteger(ArrayList<Integer> list, 
+			int min, int max, int size) {
+		if(list == null) {
+			throw new RuntimeException("리스트가 비었습니다.");
+		}
+		if(min > max) {
+			int tmp = min;
+			min = max;
+			max = tmp;
+		}
+		if(max - min + 1 < size) {
+			throw new RuntimeException("중복되지 않게 만들수 없습니다.");
+		}
+		list.clear();
+		Scanner scan = new Scanner(System.in);
+		//중복 포함해서 3개 입력
+		while(list.size() < size) {
+			list.add(scan.nextInt());
+		}
+		
+		HashSet<Integer> set = new HashSet<Integer>();
+		//입력받은 수를 set에 저장
+		set.addAll(list);
+		//중복이 안되면 true, 중복이 되면(set과 list의 개수가 다름) false
+		return list.size() == set.size();
+		
+	}
+	//list에 min~max사이의 중복되지 않은 수 size개를 저장하는 메서드
+	public static void generateRandomList(ArrayList<Integer>list,
+			int min, int max, int size) {
+		if(list == null) {
+			throw new RuntimeException("리스트가 비었습니다.");
+		}
+		if(min > max) {
+			int tmp = min;
+			min = max;
+			max = tmp;
+		}
+		if(max - min + 1 < size) {
+			throw new RuntimeException("중복되지 않게 만들수 없습니다.");
+		}
+		while(list.size() < size) {
+			int r = random(min, max);
+			//중복되지 않으면 추가
+			if(!list.contains(r)) {
+				list.add(r);
+			}
+		}
+		
+	}
+	
+	public static int random(int min, int max) {
+		if(min > max) {
+			int tmp = min;
+			min = max;
+			max = tmp;
+		}
+		Random r = new Random();
+		return r.nextInt(min, max + 1);
+	}
+	
+	public static void printResult(int strike, int ball) {
+		if(strike != 0) {
+			System.out.print(strike + "S");
+		}
+		if(ball != 0) {
+			System.out.print(ball + "B");
+		}
+		if(strike == 0 && ball == 0) {
+			System.out.print("O");
+		}
+		System.out.println();
 	}
 
-    public static int[] getUserChoice(Scanner scan) {
-        int [] userChoice = new int[3];
-        System.out.print("세개의 답을 입력해주세요: ");
-        for (int i = 0; i < 3; i++) {
-            userChoice[i] = scan.nextInt();
-        }
-        return userChoice;
-    }
-    public static int[] generateAnswer() {
-        int [] answer = new int[3];
-        for (int i = 0; i < 3; i++) {
-            outer: do {
-                answer[i] = (int) (Math.random() * 9 + 1);
-                for (int j = 0; j < i; j++) {
-                    if (answer[i] == answer[j]) {
-                        continue outer;
-                    }
-                }
-                break;
-            } while(true);
-        }
-        return answer;
-    }
-    public static int[] ballCheck(int [] answer, int [] userChoice) {
-        int strike = 0;
-        int ball = 0;
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                if (answer[i] == userChoice[j]) {
-                    if (i == j) { strike++; }
-                    else { ball++; }
-                }
-            }
-        }
-        return new int[]{ strike, ball };
-    }
-
-    public static boolean isStrikeOut(int [] ballCount) {
-        int strike = ballCount[0];
-        int ball = ballCount[1];
-
-        if (strike == 3) {
-            System.out.println("정답입니다!");
-            return true;
-        } else {
-            System.out.println(strike + "S " + ball + "B 입니다.");
-            return false;
-        }
-    }
 }
